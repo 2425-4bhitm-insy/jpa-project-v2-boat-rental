e3a57f1240c33de423ccbb8d0aebdee6f776d7bc
LSchoenbauer <l.schoenbauer@students.htl-leonding.ac.at>
2025-04-01 10:10:27 +0200

    add: attributes in employee class

--- boat-rental/src/main/java/htl/leonding/rental/entity/Employee.java
+++ boat-rental/src/main/java/htl/leonding/rental/entity/Employee.java
@@ -1,5 +1,61 @@
 package htl.leonding.rental.entity;
 
+import jakarta.persistence.*;
+
+@Entity(name = "br_employee")
 public class Employee {
-    // TODO: add attributes
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long employee_id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "role")
+    private String role;
+
+    @Column(name = "telephoneNumber")
+    private String telephoneNumber;
+
+    public Employee() {
+
+    }
+
+    public long getEmployeeId() {
+        return employee_id;
+    }
+
+    public long getEmplyeeId() {
+        return employee_id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getTelephoneNumber() {
+        return telephoneNumber;
+    }
+
+    public void setTelephoneNumber(String telephoneNumber) {
+        this.telephoneNumber = telephoneNumber;
+    }
+
+    public Employee(String name, String role, String telephoneNumber) {
+        setName(name);
+        setRole(role);
+        setTelephoneNumber(telephoneNumber);
+    }
 }
