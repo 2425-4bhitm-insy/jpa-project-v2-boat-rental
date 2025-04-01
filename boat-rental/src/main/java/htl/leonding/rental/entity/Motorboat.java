fd7cf1b0ef9c26b6db8ddeff1f53f9974155c894
elikalti14 <e.kaltenberger@students.htl-leonding.ac.at>
2025-04-01 10:33:35 +0200

    feat: add getter/setter to Motorboat.java

--- boat-rental/src/main/java/htl/leonding/rental/entity/Motorboat.java
+++ boat-rental/src/main/java/htl/leonding/rental/entity/Motorboat.java
@@ -1,4 +1,30 @@
 package htl.leonding.rental.entity;
 
-public class Motorboat {
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+
+@Entity(name = "br_motorboat")
+public class Motorboat extends Boat {
+
+    @Column(name = "motors")
+    private int motors;
+
+    @Column(name = "material")
+    private String material;
+
+    public int getMotors() {
+        return motors;
+    }
+
+    public void setMotors(int motors) {
+        this.motors = motors;
+    }
+
+    public String getMaterial() {
+        return material;
+    }
+
+    public void setMaterial(String material) {
+        this.material = material;
+    }
 }
