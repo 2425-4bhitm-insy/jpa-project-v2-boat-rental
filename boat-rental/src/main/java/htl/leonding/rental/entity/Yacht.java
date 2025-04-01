f99a9ead80a6381ee779b686053e125e6cd4474e
elikalti14 <e.kaltenberger@students.htl-leonding.ac.at>
2025-04-01 10:11:39 +0200

    feat: add getter/setter to Yacht.java

--- boat-rental/src/main/java/htl/leonding/rental/entity/Yacht.java
+++ boat-rental/src/main/java/htl/leonding/rental/entity/Yacht.java
@@ -1,4 +1,31 @@
 package htl.leonding.rental.entity;
 
-public class Yacht {
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+
+@Entity(name = "br_yacht")
+public class Yacht extends Boat {
+
+    @Column(name = "rooms")
+    private int rooms;
+
+    @Column(name = "extraBoats")
+    private int extraBoats;
+
+    public int getRooms() {
+        return rooms;
+    }
+
+    public void setRooms(int rooms) {
+        this.rooms = rooms;
+    }
+
+    public int getExtraBoats() {
+        return extraBoats;
+    }
+
+    public void setExtraBoats(int extraBoats) {
+        this.extraBoats = extraBoats;
+    }
+
 }
