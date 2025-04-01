35f6e7341693400e7a8fe5a7a59d9472cc02a02f
thiszoe <z.oellinger@students.htl-leonding.ac.at>
2025-04-01 09:31:13 +0200

    fix: make classes abstract and add inheritance strategy

--- boat-rental/src/main/java/htl/leonding/rental/entity/Boat.java
+++ boat-rental/src/main/java/htl/leonding/rental/entity/Boat.java
@@ -3,9 +3,11 @@ package htl.leonding.rental.entity;
 import jakarta.persistence.*;
 
 import java.math.BigInteger;
-
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
 @Entity(name = "br_boat")
-public class Boat {
+public abstract class Boat {
+
+    //TODO Add attributes and methodes
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
---
+++ boat-rental/src/main/java/htl/leonding/rental/entity/RentalItems.java
@@ -0,0 +1,23 @@
+package htl.leonding.rental.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Inheritance;
+import jakarta.persistence.InheritanceType;
+
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
+@Entity(name = "br_rentalItems")
+public abstract class RentalItems {
+
+    //TODO Add attributes and methodes
+    @Id
+    private Long id;
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+}
