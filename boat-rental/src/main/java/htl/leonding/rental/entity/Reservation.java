8a89d1693be26406674949af20573645621d5868
thiszoe <z.oellinger@students.htl-leonding.ac.at>
2025-04-01 10:37:49 +0200

    feat: create composite key, attributes, getter, setter and constructor in Reservation

--- boat-rental/src/main/java/htl/leonding/rental/entity/Reservation.java
+++ boat-rental/src/main/java/htl/leonding/rental/entity/Reservation.java
@@ -1,13 +1,70 @@
 package htl.leonding.rental.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
-@Entity
+import java.time.LocalDate;
+
+@Entity(name = "br_reservation")
 public class Reservation {
+
+    @ManyToOne
+    private Customer customer;
+
+    @ManyToOne
+    private Employee employee;
+
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
+    @Column(name = "startDate")
+    private LocalDate startDate;
+
+    @Column(name = "endDate")
+    private LocalDate endDate;
+
+    public Reservation(Customer customer, Employee employee, LocalDate startDate, LocalDate endDate) {
+       setCustomer(customer);
+       setEndDate(endDate);
+       setEmployee(employee);
+       setStartDate(startDate);
+    }
+
+    public Reservation() {
+
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    public Employee getEmployee() {
+        return employee;
+    }
+
+    public void setEmployee(Employee employee) {
+        this.employee = employee;
+    }
+
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(LocalDate startDate) {
+        this.startDate = startDate;
+    }
+
+    public LocalDate getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(LocalDate endDate) {
+        this.endDate = endDate;
+    }
 
     //TODO add attributes
 }
