581ad951518e78dd52eaad0fe9b2c88eab3e8c93
thiszoe <z.oellinger@students.htl-leonding.ac.at>
2025-04-01 09:57:18 +0200

    feat: add attributes, keys, getter and setter

--- boat-rental/src/main/java/htl/leonding/rental/entity/Payment.java
+++ boat-rental/src/main/java/htl/leonding/rental/entity/Payment.java
@@ -1,5 +1,48 @@
 package htl.leonding.rental.entity;
 
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+@Entity
 public class Payment {
-    //TODO add attributes
+
+    @ManyToOne
+    @JoinColumn
+    Reservation reservation;
+
+    @Column (name = "amount")
+    private double amount;
+
+    @Column(name = "paymentDate")
+    private LocalDate paymentDate;
+
+    @Id
+    private Long id;
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public LocalDate getPaymentDate() {
+        return paymentDate;
+    }
+
+    public void setPaymentDate(LocalDate paymentDate) {
+        this.paymentDate = paymentDate;
+    }
+
+    //TODO add methodes
 }
