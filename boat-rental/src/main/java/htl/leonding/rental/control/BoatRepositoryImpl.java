70aaee5e00d3b55d4108bbdeb18501a6f60c624c
elikalti14 <e.kaltenberger@students.htl-leonding.ac.at>
2025-04-20 11:33:18 +0200

    feat: add methods to BoatRepositoryImpl.java

--- boat-rental/src/main/java/htl/leonding/rental/control/BoatRepositoryImpl.java
+++ boat-rental/src/main/java/htl/leonding/rental/control/BoatRepositoryImpl.java
@@ -1,4 +1,25 @@
 package htl.leonding.rental.control;
 
-public class BoatRepositoryImpl {
+import htl.leonding.rental.entity.Boat;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.persistence.EntityManager;
+
+@ApplicationScoped
+public class BoatRepositoryImpl implements BoatRepository {
+
+    private final EntityManager entityManager;
+
+    public BoatRepositoryImpl(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    @Override
+    public void add(Boat boat) {
+        entityManager.persist(boat);
+    }
+
+    @Override
+    public Boat getBoat(Long boatId) {
+        return entityManager.find(Boat.class, boatId);
+    }
 }
