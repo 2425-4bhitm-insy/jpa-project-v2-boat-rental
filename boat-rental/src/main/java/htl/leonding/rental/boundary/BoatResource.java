42b20eeb37b1b2c33fab461f777ffc52d3baf40d
elikalti14 <e.kaltenberger@students.htl-leonding.ac.at>
2025-04-26 13:44:00 +0200

    feat: add methode getAllBoats to project

--- boat-rental/src/main/java/htl/leonding/rental/boundary/BoatResource.java
+++ boat-rental/src/main/java/htl/leonding/rental/boundary/BoatResource.java
@@ -1,4 +1,29 @@
 package htl.leonding.rental.boundary;
 
+import htl.leonding.rental.control.BoatRepositoryImpl;
+import htl.leonding.rental.entity.Boat;
+import jakarta.inject.Inject;
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+
+import java.util.List;
+
+@Path("boat")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
 public class BoatResource {
+
+    @Inject
+    BoatRepositoryImpl boatRepository;
+
+    @GET
+    @Path("getAllBoats")
+    public List<Boat> getAllBoats() {
+        return boatRepository.getAllBoats();
+    }
+
 }
