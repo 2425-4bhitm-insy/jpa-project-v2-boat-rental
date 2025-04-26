e5eea367ecda5f37c25b724ed5b882cf4d4bc757
elikalti14 <e.kaltenberger@students.htl-leonding.ac.at>
2025-04-26 12:28:32 +0200

    feat: create new Methods in BoatRepository.java

--- boat-rental/src/main/java/htl/leonding/rental/control/BoatRepository.java
+++ boat-rental/src/main/java/htl/leonding/rental/control/BoatRepository.java
@@ -1,10 +1,13 @@
 package htl.leonding.rental.control;
 
 import htl.leonding.rental.entity.Boat;
+import java.util.List;
 
 import java.util.Map;
 
 public interface BoatRepository {
     void add(Boat boat);
+    void remove(Boat boat);
     Boat getBoat(Long boatId);
+    List<Boat> getAllBoats();
 }
