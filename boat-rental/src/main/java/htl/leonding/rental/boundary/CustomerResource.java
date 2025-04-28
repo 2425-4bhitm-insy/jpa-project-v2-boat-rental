79bdfb66a611f8e49f3b5916f480d794e8a2ae42
LSchoenbauer <l.schoenbauer@students.htl-leonding.ac.at>
2025-04-28 20:22:40 +0200

    feat: Customer Resource

---
+++ boat-rental/src/main/java/htl/leonding/rental/boundary/CustomerResource.java
@@ -0,0 +1,35 @@
+package htl.leonding.rental.boundary;
+
+import htl.leonding.rental.control.CustomerRepository;
+import htl.leonding.rental.entity.Customer;
+import jakarta.inject.Inject;
+import jakarta.transaction.Transactional;
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+
+@Path("/customers")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
+public class CustomerResource {
+
+    @Inject
+    CustomerRepository customerRepository;
+
+    @GET
+    @Path("/{id}")
+    public Response getCustomer(@PathParam("id") Long id) {
+        Customer customer = customerRepository.getCustomer(id);
+        if (customer == null) {
+            return Response.status(404, "Customer with id '" + id + "' was not found.").build();
+        }
+        return Response.ok(customer).build();
+    }
+
+    @POST
+    @Transactional
+    public Response addCustomer(Customer customer) {
+        customerRepository.add(customer);
+        return Response.status(201).entity(customer).build();
+    }
+}
