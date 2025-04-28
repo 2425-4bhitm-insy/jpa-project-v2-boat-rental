bb99571cd009d45381049b7e42a54188a3eb97ae
thiszoe <z.oellinger@students.htl-leonding.ac.at>
2025-04-28 21:39:22 +0200

    Test: testing constructor, getter and setter

--- boat-rental/src/test/java/htl/leonding/rental/LeaseTest.java
+++ boat-rental/src/test/java/htl/leonding/rental/LeaseTest.java
@@ -1,4 +1,46 @@
 package htl.leonding.rental;
 
+import htl.leonding.rental.entity.Lease;
+import htl.leonding.rental.entity.RentalItems;
+import htl.leonding.rental.entity.Reservation;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class LeaseTest {
+
+    // Dummy-Klassen für Testzwecke
+    static class DummyReservation extends Reservation {
+    }
+
+    static class DummyRentalItem extends RentalItems {
+    }
+
+    DummyReservation reservation = new DummyReservation();
+    DummyRentalItem rentalItem = new DummyRentalItem();
+    Lease lease = new Lease(reservation, rentalItem);
+
+    @Test
+    void testConstructorAndGetters() {
+        assertEquals(reservation, lease.getReservation());
+        assertEquals(rentalItem, lease.getRentalItems());
+    }
+
+    @Test
+    void testSetReservation() {
+        DummyReservation newReservation = new DummyReservation();
+        lease.setReservation(newReservation);
+        assertEquals(newReservation, lease.getReservation());
+    }
+
+    @Test
+    void testSetRentalItems() {
+        DummyRentalItem newRentalItem = new DummyRentalItem();
+        lease.setRentalItems(newRentalItem);
+        assertEquals(newRentalItem, lease.getRentalItems());
+    }
+
+
 }
+
