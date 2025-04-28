3067db08ecff4e3aee5f56fd5be56d6332234010
LSchoenbauer <l.schoenbauer@students.htl-leonding.ac.at>
2025-04-28 03:50:13 +0200

    feat: Lease Repository Implementation

---
+++ boat-rental/src/main/java/htl/leonding/rental/control/LeaseRepositoryImpl.java
@@ -0,0 +1,34 @@
+package htl.leonding.rental.control;
+
+import htl.leonding.rental.entity.Lease;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import jakarta.persistence.EntityManager;
+
+import java.util.List;
+
+@ApplicationScoped
+public class LeaseRepositoryImpl implements LeaseRepository {
+
+    @Inject EntityManager entityManager;
+
+    @Override
+    public void add(Lease lease) {
+        entityManager.persist(lease);
+    }
+
+    @Override
+    public void remove(Lease lease) {
+        entityManager.remove(entityManager.contains(lease) ? lease : entityManager.merge(lease));
+    }
+
+    @Override
+    public Lease getLease(Long leaseId) {
+        return entityManager.find(Lease.class, leaseId);
+    }
+
+    @Override
+    public List<Lease> getAllLeases() {
+        return entityManager.createQuery("SELECT l FROM br_lease l", Lease.class).getResultList();
+    }
+}
