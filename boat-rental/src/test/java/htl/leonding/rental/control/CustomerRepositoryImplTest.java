5f79c9ed2ebdd6b12d10193643f1ea120c810693
thiszoe <z.oellinger@students.htl-leonding.ac.at>
2025-04-28 22:25:15 +0200

    Test: testing for add and get customer

--- boat-rental/src/test/java/htl/leonding/rental/control/CustomerRepositoryImplTest.java
+++ boat-rental/src/test/java/htl/leonding/rental/control/CustomerRepositoryImplTest.java
@@ -1,5 +1,47 @@
 package htl.leonding.rental.control;
 
-public class CustomerRepositoryImplTest {
+import htl.leonding.rental.entity.Customer;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+import org.junit.jupiter.api.*;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class CustomerRepositoryImplTest {
+    static EntityManagerFactory emf = Persistence.createEntityManagerFactory("testPU");
+    EntityManager em = emf.createEntityManager();
+    CustomerRepositoryImpl repository = new CustomerRepositoryImpl(em);
+
+    @BeforeEach
+    void setUp() {
+        em.getTransaction().begin();
+    }
+
+    @AfterEach
+    void tearDown() {
+        if (em.getTransaction().isActive()) {
+            em.getTransaction().rollback();
+        }
+        em.close();
+    }
+
+    @AfterAll
+    static void close() {
+        emf.close();
+    }
+
+    @Test
+    void testAddAndGetCustomer() {
+        Customer customer = new Customer();
+        customer.setFirstname("Max");
+
+        repository.add(customer);
+        em.flush(); // wichtig, um persistierte Objekte wirklich in die DB zu schreiben
+
+        Customer found = repository.getCustomer(customer.getCustomerId());
+        assertNotNull(found);
+        assertEquals("Max", found.getFirstname());
+    }
 }
